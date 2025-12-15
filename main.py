e7bfbada31f37bef87220a66e2c340ef22411b12
Unique Subedi <subediunique12@gmail.com>
2025-12-15 21:08:27 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+'''
+1 for Snake
+-1 for Water
+0 for gun
+'''
+computer=random.choice([-1,0,1])
+youstr=input("Enter your Choice:")
+if youstr not in ["s", "w", "g"]:
+    print("Invalid choice! Please enter s, w, or g.")
+    exit()
+
+youDict={"s": 1, "w": -1, "g": 0}
+reverseDict={1: "Snake",-1: "Water",0: "Gun"}
+
+you = youDict[youstr]
+
+print(f"You choose {reverseDict[you]}\nComputer Choose {reverseDict[computer]}")
+
+if(computer==you):
+    print("Its a draw.")
+else:
+    if(computer==-1 and you==1):
+     print("You Win!")
+
+    elif(computer==-1 and you==0):
+     print("You Lose!")
+
+    elif(computer==1 and you==-1):
+     print("You Lose!")
+    elif(computer==1 and you==0):
+     print("You Win!")
+    elif(computer==0 and you==-1):
+     print("You Win!")
+    elif(computer==0 and you==1):
+     print("You Lose!")
+    else:
+     print("Something went Wrong!")
+
